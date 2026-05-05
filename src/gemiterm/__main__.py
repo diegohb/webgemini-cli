e9ba519b6ae77954bf7f525efc1ede12b23ade61
diegohb <support@expertvision.software>
2026-05-04 22:08:10 -0400

    Refactor main execution block to improve error handling for NoArgsIsHelpError

--- src/gemiterm/__main__.py
+++ src/gemiterm/__main__.py
@@ -1,7 +1,12 @@
 import asyncio
+from click.exceptions import NoArgsIsHelpError
+
 from gemiterm.cli import cli
 
 if __name__ == "__main__":
-    result = cli(standalone_mode=False)
-    if asyncio.iscoroutine(result):
-        asyncio.run(result)
+    try:
+        result = cli(standalone_mode=False)
+        if asyncio.iscoroutine(result):
+            asyncio.run(result)
+    except NoArgsIsHelpError:
+        cli.main(["--help"])
